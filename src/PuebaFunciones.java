d836778e77339afba4ceb60a124b1d4f362722d6
Jamzin <programajazminc20@gmail.com>
2025-11-26 09:03:53 +0100

    Uso de funciones del profe, prueba correccion del orden

--- src/PuebaFunciones.java
+++ src/PuebaFunciones.java
@@ -2,16 +2,65 @@ import matematicas.*;
 
 public class PuebaFunciones {
     public static void main(String[] args) throws Exception {
+        // 1. esCapicua
+        System.out.println("El 464 "+(Funciones.esCapicua(464)?"es capicúa":"no es capicúa"));
 
+        // 2. esPrimo
+        System.out.println("El 29 "+(Funciones.esPrimo(29)?"es primo":"no es primo"));
 
-        System.out.printf("El número %d %s capicua%n", 464, Funciones.esCapicua(464) ? "es" : "no es");
+        // 3. siguientePrimo
+        System.out.println("El siguiente primo mayor a 23 es " + Funciones.siguientePrimo(23));
+        System.out.println("El siguiente primo mayor a 100 es " + Funciones.siguientePrimo(100));
 
-        System.out.printf("El número %d %s primo%n",29,Funciones.esPrimo(29) ? "es" : "no es");
+        // 4. potencia
+        System.out.println("2^10 = " + Funciones.potencia(2, 10));
+        System.out.println("5^(-3) = " + Funciones.potencia(5, -3));
+        System.out.println("10^6 = " + Funciones.potencia(10, 6));
 
-        System.out.printf("El siguiente primo de %d es %d%n", 23, Funciones.siguientePrimo(23));
+        // 5. digitos
+        System.out.println("El número 0 tiene " + Funciones.digitos(0) + " dígito/s.");
+        System.out.println("El número 7 tiene " + Funciones.digitos(7) + " dígito/s.");
+        System.out.println("El número 674893123 tiene " + Funciones.digitos(674893123) + " dígito/s.");
 
-        System.out.printf("El siguiente primo de %d es %d%n", 100, Funciones.siguientePrimo(100));
+        // 6. voltea
+        System.out.println("El 5 volteado es " + Funciones.voltea(5));
+        System.out.println("El 398004321 volteado es " + Funciones.voltea(398004321));
+        System.out.println("El -75839 volteado es " + Funciones.voltea(-75839));
 
-        System.out.printf("%d%n", Funciones.quitaPorDelante(78604111, 2));
-    }   
+        // 7. digitoN
+        System.out.println("En la posición 0 del 3452 está el " + Funciones.digitoN(3452, 0));
+        System.out.println("En la posición 6 del 857964034 está el " + Funciones.digitoN(857964034, 6));
+        System.out.println("En la posición 4 del 857964034 está el " + Funciones.digitoN(857964034, 4));
+
+        // 8. posicionDeDigito
+        System.out.println("En el 3452, el dígito 4 está en la posición " + Funciones.posicionDeDigito(3452, 4));
+        System.out.println("En el 78604321, el dígito 1 está en la posición " + Funciones.posicionDeDigito(78604321, 1));
+        System.out.println("En el 78604321, el dígito 7 está en la posición " + Funciones.posicionDeDigito(78604321, 7));
+        System.out.println("En el 78604321, el dígito 5 está en la posición " + Funciones.posicionDeDigito(78604321, 5));
+
+        // 9. quitaPorDetras
+        System.out.println("Si al 78604321 se le quitan por detrás 4 dígitos, se queda como " + Funciones.quitaPorDetras(78604321, 4));
+        System.out.println("Si al 1000 se le quita por detrás 1 dígito, se queda como " + Funciones.quitaPorDetras(1000, 1));
+
+        // 10. quitaPorDelante
+        System.out.println("Si al 78604321 se le quitan por delante 4 dígitos, se queda como " + Funciones.quitaPorDelante(78604321, 4));
+        System.out.println("Si al 78604000 se le quitan por delante 2 dígitos, se queda como " + Funciones.quitaPorDelante(78604000, 2));
+
+        // 11. pegaPorDetras
+        System.out.println("Si al 567 se le pega por detrás el 1 da el " + Funciones.pegaPorDetras(567, 1));
+        System.out.println("Si al 33 se le pega por detrás el 0 da el " + Funciones.pegaPorDetras(33, 0));
+
+        // 12. pegaPorDelante
+        System.out.println("Si al 567 se le pega por delate el 1 da el " + Funciones.pegaPorDelante(567, 1));
+        System.out.println("Si al 33 se le pega por delante el 0 da el " + Funciones.pegaPorDelante(33, 0));
+
+        // 13. trozoDeNumero
+        System.out.println("Al 78604000 le cojo el trozo que va de la posición 0 a la 3: " + Funciones.trozoDeNumero(78604000, 0, 3));
+        System.out.println("Al 78604000 le cojo el trozo que va de la posición 4 a la 6: " + Funciones.trozoDeNumero(78604000, 4, 6));
+        System.out.println("Al 78604000 le cojo el trozo que va de la posición 2 a la 2: " + Funciones.trozoDeNumero(78604000, 2, 2));
+
+        // 14. juntaNumeros
+        System.out.println("Juntando el 21 y el 40 da el " + Funciones.juntaNumeros(21, 40));
+        System.out.println("Juntando el 789 y el 250 da el " + Funciones.juntaNumeros(789, 250));
+    }
 }
--- src/matematicas/Funciones.java
+++ src/matematicas/Funciones.java
@@ -2,182 +2,381 @@ package matematicas;
 
 public class Funciones {
 
-     /**
-      * Comprueba si un numero es capicua
-      * @param numero
-      * @return
-      */
-    public static boolean esCapicua (int numero) {
-        boolean esCapicua =  false;
-        if (numero == voltea(numero)) {
-            esCapicua = true;
-        }
-        return esCapicua;
+//      /**
+//       * Comprueba si un numero es capicua
+//       * @param numero
+//       * @return
+//       */
+//     public static boolean esCapicua (int numero) {
+//         boolean esCapicua =  false;
+//         if (numero == voltea(numero)) {
+//             esCapicua = true;
+//         }
+//         return esCapicua;
+//     }
+
+//     /**
+//      * Devuelve el siguiente numero primo de un numero dado por parametro
+//      * @param numero
+//      * @return
+//      */
+//     public static int siguientePrimo (int numero) {
+//         int numeroSiguientePrimo = 0;
+//         boolean esPrimoSiguiente = false;
+//         int pruebaNum = numero + 1;
+
+//         while (!esPrimoSiguiente) {
+//             esPrimoSiguiente = esPrimo(pruebaNum);
+//             if (esPrimoSiguiente) {
+//                 numeroSiguientePrimo = pruebaNum;
+//             } else {
+//                 pruebaNum++;
+//             }
+//         }
+//         return numeroSiguientePrimo;
+//     }
+
+//     /**
+//      * Comprueba si un numero es primo
+//      * @param numero
+//      * @return
+//      */
+//     public static boolean esPrimo (int numero) {
+//         boolean esPrimo = true;
+//         for (int i = 2; i <= numero / 2; i++) {
+//             if (numero % i == 0) {
+//                 esPrimo = false;
+//             }
+//         }
+//         return esPrimo;
+//     }
+    
+//     /**
+//      * Devuelve la posicion de un digito en un numero
+//      * @param numero
+//      * @param digitoBuscado
+//      * @return
+//      */
+//     public static int posicionDeDigito (int numero, int digitoBuscado) {
+//         int posicion = -1;
+//         int posicionActual = 0;
+
+//         while (numero != 0) {
+//             int digito = numero % 10;
+//             if (digito == digitoBuscado) {
+//                 posicion = digitos(numero);
+//             }
+//             numero /= 10;
+//             posicionActual++;
+//         }
+
+//         return posicion;
+//     }
+
+
+//     /**
+//      * Quita por detras
+//      * @param numero
+//      * @param cantidad
+//      * @return
+//      */
+//     public static int quitaPorDetras (int numero, int cantidad) {
+//         while (cantidad > 0) {
+//             numero /= 10;
+//             cantidad--;
+//         }
+//         return numero;
+//     }
+
+//     /**
+//      * Quita por delante
+//      * @param numero
+//      * @param cantidad
+//      * @return
+//      */
+//     public static int quitaPorDelante (int numero, int cantidad) {
+//         numero = voltea(numero);
+//         while (cantidad > 0) {
+//             numero /= 10;
+//             cantidad--;
+//         }
+//         numero = voltea(numero);
+//         return numero;
+//     }
+
+//     /**
+//      * Pega por detras
+//      * @param numero
+//      * @param digito
+//      * @return
+//      */
+//     public static int pegaPorDetras (int numero, int digito) {
+//         numero = (numero * 10) + digito;
+//         return numero;
+//     }
+
+//     /**
+//      * Pega por delante
+//      * @param numero
+//      * @param digito
+//      * @return
+//      */
+//     public static int pegaPorDelante (int numero, int digito) {
+//         int volteadoN = voltea(numero);
+//         volteadoN = pegaPorDetras(volteadoN, digito);
+//         return voltea(volteadoN);
+//     }
+
+//     /**
+//      * Voltea un numero
+//      * @param numero
+//      * @return
+//      */
+
+//     /**
+//      * Devuelve el digito que está en la posición n de un numero entero, contando de izquierda a dercha
+//      * @param numero
+//      * @param posicion
+//      * @return
+//      */
+//     public static int digitoN (int numero, int posicion) {
+//         int digitoN = 0;
+//         int digito = 0;
+//         numero = voltea(numero);
+//         while (numero != 0) {
+//             digito = numero % 10;
+//             if (digitoN == posicion) {
+//                 digitoN = digito;
+//             }
+//             numero /= 10;
+//         }
+//         return digitoN;
+//     }
+
+    
+//     /**
+//      * Voltea un numero
+//      * @param numero
+//      * @return
+//      */
+//     public static int voltea (int numero) {
+
+//         int numeroVolteado = 0;
+
+//         while (numero != 0) {
+//             int digito = numero % 10;
+//             numeroVolteado = (numeroVolteado * 10) + digito;
+//             numero /= 10;
+//         }
+
+//         return numeroVolteado;
+//     }
+
+//     /**
+//      * Cuenta los digitos de un numero
+//      * @param numero
+//      * @return
+//      */
+//     public static int digitos (int numero) {
+//         int digitos = 0;
+
+//         while (numero != 0) {
+//             numero /= 10;
+//             digitos++;
+//         }
+
+//         return digitos;
+//     }
+
+//     /**
+//      * Calcula la potencia de un numero
+//      * @param base
+//      * @param exponente
+//      * @return
+//      */
+
+//     public static double potencia (double base, int exponente) {
+//         double potencia = 1;
+
+//         for (int i = 0; i < exponente; i++) {
+//             potencia *= base;
+//         }
+
+//         return potencia;
+//     }
+// }
+
+    // ─────────────────────────────────────────────
+    //  FUNCIONES COMPLEJAS
+    // ─────────────────────────────────────────────
+
+    public static boolean esCapicua(int numero){
+        return esCapicua((long)numero);
     }
 
-    /**
-     * Devuelve el siguiente numero primo de un numero dado por parametro
-     * @param numero
-     * @return
-     */
-    public static int siguientePrimo (int numero) {
-        int numeroSiguientePrimo = 0;
-        boolean esPrimoSiguiente = false;
-        int pruebaNum = numero + 1;
-
-        while (!esPrimoSiguiente) {
-            esPrimoSiguiente = esPrimo(pruebaNum);
-            if (esPrimoSiguiente) {
-                numeroSiguientePrimo = pruebaNum;
-            } else {
-                pruebaNum++;
-            }
-        }
-        return numeroSiguientePrimo;
+    public static boolean esCapicua(long numero){
+        return numero == voltea(numero);
     }
 
-    /**
-     * Comprueba si un numero es primo
-     * @param numero
-     * @return
-     */
-    public static boolean esPrimo (int numero) {
-        boolean esPrimo = true;
-        for (int i = 2; i <= numero / 2; i++) {
-            if (numero % i == 0) {
-                esPrimo = false;
-            }
+    public static int voltea(int numero){
+        return (int) voltea((long) numero);
+    }
+
+    public static long voltea(long numero){
+        boolean negativo = numero < 0;
+        numero = Math.abs(numero);
+        int volteado = 0;
+        for(int i=1; i<=digitos(numero);i++)
+            volteado = pegaPorDetras(volteado, digitoN(numero, digitos(numero)-i));
+        return (negativo)?-volteado:volteado;
+    }
+
+    public static int posicionDeDigito(int numero, int digito){
+        return posicionDeDigito((long)numero, digito);
+    }
+
+    public static int posicionDeDigito(long numero, int digito){
+        for(int i=0; i<digitos(numero);i++){
+            if(digitoN(numero, i)==digito)
+                return i;
         }
-        return esPrimo;
+        return -1;
+    }
+
+    public static int digitoN(int numero, int posicion){
+        return (int) trozoDeNumero(numero, posicion, posicion);
+    }
+
+    public static int digitoN(long numero, int posicion){
+        return (int) trozoDeNumero(numero, posicion, posicion);
+    }
+
+    public static int pegaPorDelante(int numero, int digito){
+        return (int) pegaPorDelante((long) numero, digito);
+    }
+
+    public static long pegaPorDelante(long numero, int digito){
+        return juntaNumeros(digito, numero);
+    }     
+
+    public static int juntaNumeros(int numero1, int numero2){
+        return (int) juntaNumeros((long) numero1, (long) numero2);
+    }
+
+    public static long juntaNumeros(long numero1, long numero2){
+        int digitosSegundo = digitos(numero2);
+        for(int i=0;i<digitosSegundo;i++){            
+            numero1 = pegaPorDetras(numero1, digitoN(numero2, i));            
+        }
+        return numero1;
+    }
+
+    public static int quitaPorDetras(int numero, int digitos){
+        return (int) quitaPorDetras((long)numero, digitos);
+    }
+
+    public static int quitaPorDelante(int numero, int digitos){
+        return (int) quitaPorDelante((long)numero, digitos);
+    }
+
+    public static int trozoDeNumero(int numero, int inicio, int fin){                
+        return (int) trozoDeNumero((long)numero, inicio, fin);
+    }
+
+    public static long trozoDeNumero(long numero, int inicio, int fin){
+        if(inicio <= fin){            
+            numero = quitaPorDetras(numero, digitos(numero)-fin-1);
+            return quitaPorDelante(numero, inicio);
+        }
+        return numero;
+    }
+
+    // ─────────────────────────────────────────────
+    //  PRIMOS
+    // ─────────────────────────────────────────────
+
+    public static boolean esPrimo(int numero){
+        return esPrimo((long)numero);
+    }
+
+    public static short siguientePrimo(short numero){
+        return (short) siguientePrimo((long)numero);
+    }
+
+    public static int siguientePrimo(int numero){
+        return (int) siguientePrimo((long)numero);
+    }
+
+    public static long siguientePrimo(long numero){
+        while(!esPrimo(++numero)){}
+        return numero;
+    }
+
+    public static boolean esPrimo(long numero){
+        for(int i = 2; i<=numero/2; i++){
+            if(numero % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    // ─────────────────────────────────────────────
+    //  DIGITOS
+    // ─────────────────────────────────────────────
+
+    public static int digitos(int numero){
+        return digitos((long)numero);
+    }
+
+    public static int digitos(long numero){
+        numero = Math.abs(numero);
+        int digitos = 0;
+        do{
+            digitos++;
+            numero/=10;
+        }
+        while(numero>0);
+        return digitos;            
+    }
+
+    // ─────────────────────────────────────────────
+    //  PEGAR Y QUITAR DÍGITOS
+    // ─────────────────────────────────────────────
+
+    public static int pegaPorDetras(int numero, int digito){
+        return (int) pegaPorDetras((long)numero, digito);
+    }
+
+    public static long pegaPorDetras(long numero, int digito){
+        return numero*10+digito;
+    }
+
+    public static long quitaPorDetras(long numero, int digitos){
+        return numero/(long)potencia(10,digitos);
+    }
+
+    public static long quitaPorDelante(long numero, int digitos){
+        return numero % (int)potencia(10, digitos(numero)-digitos);
+    }
+
+    // ─────────────────────────────────────────────
+    //  POTENCIAS
+    // ─────────────────────────────────────────────
+
+    public static float potencia(float base, int exponente){
+        return (float) potencia((double)base, exponente);
+    }
+
+    public static double potencia(double base, int exponente){        
+        return (exponente<0)?(double)1/_potencia(base, Math.abs(exponente)):_potencia(base, exponente);
+    }
+
+    private static double _potencia(double base, int exponente){
+        double res = 1;
+        for(int i=0;i<exponente;i++){
+            res*=base;
+        }
+        return res;
     }
     
-    /**
-     * Devuelve la posicion de un digito en un numero
-     * @param numero
-     * @param digitoBuscado
-     * @return
-     */
-    public static int posicionDeDigito (int numero, int digitoBuscado) {
-        int posicion = -1;
-        int posicionActual = 0;
-
-        while (numero != 0) {
-            int digito = numero % 10;
-            if (digito == digitoBuscado) {
-                posicion = digitos(numero);
-            }
-            numero /= 10;
-            posicionActual++;
-        }
-
-        return posicion;
-    }
-
-
-    /**
-     * Quita por detras
-     * @param numero
-     * @param cantidad
-     * @return
-     */
-    public static int quitaPorDetras (int numero, int cantidad) {
-        while (cantidad > 0) {
-            numero /= 10;
-            cantidad--;
-        }
-        return numero;
-    }
-
-    /**
-     * Quita por delante
-     * @param numero
-     * @param cantidad
-     * @return
-     */
-    public static int quitaPorDelante (int numero, int cantidad) {
-        numero = voltea(numero);
-        while (cantidad > 0) {
-            numero /= 10;
-            cantidad--;
-        }
-        numero = voltea(numero);
-        return numero;
-    }
-
-    //Pega por detras
-    /**
-     * Pega por detras
-     * @param numero
-     * @param digito
-     * @return
-     */
-    public static int pegaPorDetras (int numero, int digito) {
-        numero = (numero * 10) + digito;
-        return numero;
-    }
-
-    //Pega por delante
-    /**
-     * Pega por delante
-     * @param numero
-     * @param digito
-     * @return
-     */
-    public static int pegaPorDelante (int numero, int digito) {
-        int volteadoN = voltea(numero);
-        volteadoN = pegaPorDetras(volteadoN, digito);
-        return voltea(volteadoN);
-    }
-
-    /**
-     * Voltea un numero
-     * @param numero
-     * @return
-     */
-
-    public static int voltea (int numero) {
-
-        int numeroVolteado = 0;
-
-        while (numero != 0) {
-            int digito = numero % 10;
-            numeroVolteado = (numeroVolteado * 10) + digito;
-            numero /= 10;
-        }
-
-        return numeroVolteado;
-    }
-
-    /**
-     * Cuenta los digitos de un numero
-     * @param numero
-     * @return
-     */
-    public static int digitos (int numero) {
-        int digitos = 0;
-
-        while (numero != 0) {
-            numero /= 10;
-            digitos++;
-        }
-
-        return digitos;
-    }
-
-    /**
-     * Calcula la potencia de un numero
-     * @param base
-     * @param exponente
-     * @return
-     */
-
-    public static double potencia (double base, int exponente) {
-        double potencia = 1;
-
-        for (int i = 0; i < exponente; i++) {
-            potencia *= base;
-        }
-
-        return potencia;
-    }
 }
